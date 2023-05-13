c5a65248db8d27a35c4f53bf4377b0f2e70dd840
g.sen <giu.sene@gmail.com>
2023-05-13 18:57:20 +0200

    feat: next js bootstrapping

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  trailingSlash: true,
+  env: {
+    PUBLIC_URL: "/",
+  },
+  sassOptions: {
+    includePaths: ["src/styles"],
+    // prependData: `@import "./src/styles/global.scss";`,
+  },
+  // images: {
+  //   path: "public/",
+  //   unoptimized: true,
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
